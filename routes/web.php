cb0172316e087c00ff5df63935f7b9bcb4b43133
Irsyadul Ibad <116159919+mbahjoglo@users.noreply.github.com>
2024-02-12 10:23:21 +0700

    cache

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('cache:clear');
+    // return what you want
+});
 
 // ================ Home View ================
 
